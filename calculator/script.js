2501fb8f2e0d7d72125f2e48f1f3201f1e9ecfd9
Atharva Dhawane <57084914+devil970@users.noreply.github.com>
2024-05-27 23:50:29 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,20 @@
+console.log("Event Fired")
+function c(val)
+{
+document.getElementById("d").value=val;
+}
+function v(val)
+{
+document.getElementById("d").value+=val;
+}
+function e() 
+{ 
+try 
+{ 
+  c(eval(document.getElementById("d").value)) 
+} 
+catch(e) 
+{
+  c('ERRR WRONG') 
+} 
+}  
